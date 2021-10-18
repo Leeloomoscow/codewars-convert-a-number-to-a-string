ffba74471db2bd2e591990b3b3a2a0902ec746d6
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-18 20:52:17 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void tests() {
+        assertEquals("67", Kata.numberToString(67));
+        assertEquals("123", Kata.numberToString(123));
+        assertEquals("999", Kata.numberToString(999));
+    }
 
+    private static class Kata {
+        public static String numberToString(int num) {
+           String str = Integer.toString(num);
+
+            return str;
+        }
     }
 }
